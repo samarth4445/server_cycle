789eb2de2fb15e16666f4ff443d9fd80e1154062
Samarth <nimbargisamarth@gmail.com>
2022-10-03 21:01:24 +0530

    first commit

---
+++ functions/functions.py
@@ -0,0 +1,4 @@
+import random
+
+def otpGenerator():
+    return random.randint(1000, 9999)
---
+++ resources/otp.py
@@ -0,0 +1,30 @@
+from flask import request
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+from db import otp, endTrip
+from functions.functions import otpGenerator
+
+blp = Blueprint("stores", __name__, description="Operations on stores.")
+
+@blp.route("/otp_gen")
+class OtpGenerator(MethodView):
+    def post(self):
+        request_data = request.get_json()
+        otp[request_data["cycleid"]] = otpGenerator()
+        endTrip[request_data["cycleid"]] = 0
+        return {"otp": otp[request_data["cycleid"]]}, 201
+
+@blp.route("/otp_get/<int:cycleid>")
+class OtpGet(MethodView):
+    def get(self, cycleid):
+        if cycleid not in otp or otp[cycleid] is None:
+            return {"otp": None}, 404
+        return {"otp": otp[cycleid]}
+
+@blp.route("/endtrip/<int:cycleid>")
+class EndTrip(MethodView):
+    def get(self, cycleid):
+        otp[cycleid] = None
+        endTrip[cycleid] = 1
+        return {"message": "Trip ended."}
+
