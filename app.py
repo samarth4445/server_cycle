da783c7bbc6abad0630e96dcb43b4065f71dca09
samarth4445 <nimbargisamarth@gmail.com>
2022-10-09 11:21:48 +0530

    Added Geofencing

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from resources.position import blp as LocationBlueprint
 app = Flask(__name__)
 
 app.config["PROPOGATE_EXCEPTIONS"] = True
-app.config["API_TITLE"] = "GoCycle OTP System"
+app.config["API_TITLE"] = "GoCycle API"
 app.config["API_VERSION"] = "v1"
 app.config["OPENAPI_VERSION"] = "3.0.3"
 app.config["OPENAPI_URL_PREFIX"] = "/"
--- resources/position.py
+++ resources/position.py
@@ -1,3 +1,4 @@
+import geopy.distance
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
@@ -13,12 +14,26 @@ class Location(MethodView):
     
     def post(self):
         request_data = request.get_json()
+
+        centre = (18.492583, 74.0255370)
+        current_pos = (float(request_data["longitude"]), float(request_data["latitude"]))
+
+        position_magnitude = geopy.distance.geodesic(centre, current_pos).km
         cycleid = request_data["cycleid"]
+
+        if position_magnitude > 2.200408063912031:
+            for i in position:
+                if i["cycleid"] == cycleid:
+                    i["longitude"] = request_data["longitude"]
+                    i["latitude"] = request_data["latitude"]
+                    i["fine"] = True
+                    return {"message": "You are outside the university, you will be fined."}
+
         for i in position:
             if i["cycleid"] == cycleid:
                 i["longitude"] = request_data["longitude"]
                 i["latitude"] = request_data["latitude"]
                 return {"message": "Information updated."}
 
-        position.append({"longitude": request_data["longitude"], "latitude": request_data["latitude"], "cycleid": request_data["cycleid"]})
+        position.append({"longitude": request_data["longitude"], "latitude": request_data["latitude"], "cycleid": request_data["cycleid"], "fine": False})
         return {"message": "Information sent."}, 201
